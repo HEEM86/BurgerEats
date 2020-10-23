9fb1c509732b938d990b96d30364a95263ee60a5
Slice Wess <ernestwesson86@gmail.com>
2020-10-23 02:16:53 -0400

    heroku setup push

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-USE burgereats_db
+USE burgereats_db;
 
-INSERT INTO burgereats_db (name) VALUES ('BLT Burger');
-INSERT INTO burgereats_db (name) VALUES ('Big Man Burger');
-INSERT INTO burgereats_db (name) VALUES ('Mondo Burger');
+INSERT INTO burgereats_db (burger_name) VALUES ('BLT Burger');
+INSERT INTO burgereats_db (burger_name) VALUES ('Big Man Burger');
+INSERT INTO burgereats_db (burger_name) VALUES ('Mondo Burger');
 
