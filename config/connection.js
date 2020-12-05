80d6097b4a3b097c83bfb10b722822d5ed0ea9fe
Slice Wess <ernestwesson86@gmail.com>
2020-12-04 18:24:57 -0800

    working on handlebar files

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,10 @@ if (process.env.JAWSDB_URL) {
 connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 connection = mysql.createConnection({
-  host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "iuj37vf90g6fi6yw",
+  host: "local host",
+  user: "root",
   password: "Slice1986!",
-  database: "zqbkxw4qot4fn5j3"
+  database: "burgereats_db"
 });
 };
 
