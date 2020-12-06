e59c08f8bf81f0b098af0cafe0328790c3d4483c
Slice Wess <ernestwesson86@gmail.com>
2020-12-05 23:41:06 -0800

    finally getting some styling

--- controller/burgereats_controller.js
+++ controller/burgereats_controller.js
@@ -18,7 +18,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "name", "devoured"
+    "burger_name", "devoured"
   ], [
     req.body.name, req.body.devoured
   ], function(result) {
@@ -43,6 +43,19 @@ router.put("/api/burgers/:id", function(req, res) {
     }
   });
 });
+
+// router.delete("/api/burgers/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   burger.delete(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 
 module.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 USE burgereats_db;
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Burger',true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Big Man Burger',true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mondo Burger',true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Burger',TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Man Burger',TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mondo Burger',TRUE);
 
