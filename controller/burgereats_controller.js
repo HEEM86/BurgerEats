d4ec31037e56247e11affb90db0a75e35c09c8c1
Slice Wess <ernestwesson86@gmail.com>
2020-12-06 00:45:59 -0800

    changed var to const lines 1-6

--- controller/burgereats_controller.js
+++ controller/burgereats_controller.js
@@ -1,9 +1,9 @@
-var express = require("express");
+const express = require("express");
 
-var router = express.Router();
+const router = express.Router();
 
 
-var burger = require("../models/burgereats.js");
+const burger = require("../models/burgereats.js");
 
 
 router.get("/", function(req, res) {
@@ -44,18 +44,18 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
-// router.delete("/api/burgers/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
-//   burger.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 
 module.exports = router;
